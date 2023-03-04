519446eb73e117921adf28d73a3036cd2185bce6
izzatulla <itoshpulato@exadel.com>
2023-03-04 23:29:33 +0500

    fixed error logic in getPage from list

--- src/main/java/com/itoffice/security/util/Utils.java
+++ src/main/java/com/itoffice/security/util/Utils.java
@@ -3,14 +3,25 @@ package com.itoffice.security.util;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class Utils {
-    public static <T> Page<T> getPageFromList(List<T> content, int page, int size) {
-        return new PageImpl<>(content, PageRequest.of(page, size), content.size());
+    private static <T> List<T> detachPageFromList(List<T> content, int offset, int pageSize) {
+        int start = offset * pageSize;
+        int end = Math.min(pageSize + (offset * pageSize), content.size());
+        return start > end ? new ArrayList<>() : content.subList(start, end);
+    }
+
+    public static <T> Page<T> getPageFromList(List<T> content, int offset, int pageSize) {
+        List<T> page = detachPageFromList(content, offset, pageSize);
+
+        Pageable pageable = PageRequest.of(offset, pageSize);
+        return new PageImpl<>(page, pageable, content.size());
     }
 
 }
