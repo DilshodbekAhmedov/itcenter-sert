5bed175ced309381c010a2e73f4417f51fef130a
izzatulla <itoshpulato@exadel.com>
2023-03-04 15:35:44 +0500

    fixed null pointer in cell date value

--- src/main/java/com/itoffice/security/certificate/CertificateServiceImpl.java
+++ src/main/java/com/itoffice/security/certificate/CertificateServiceImpl.java
@@ -119,6 +119,9 @@ public class CertificateServiceImpl implements CertificateService {
             Sheet sheet = workbook.getSheetAt(0);
             for (Row row : sheet) {
                 if (!sheet.getRow(0).equals(row)) {
+                    if (row.getCell(2).getDateCellValue()==null){
+                        continue;
+                    }
                     Certificate certificate = new Certificate();
                     certificate.setStudentName(row.getCell(0).getStringCellValue());
                     certificate.setStudentMan(row.getCell(1).getBooleanCellValue());
